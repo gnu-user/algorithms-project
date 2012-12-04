a846aa9e742cd33aefba92335ca93eb354d3055b
Joseph <heron.joseph@gmail.com>
2012-12-04 13:18:36 -0500

    MODIFIED: fixed up some of the documenation

--- shortest-path/dijkstra_new.py
+++ shortest-path/dijkstra_new.py
@@ -1,32 +1,4 @@
-#import pdb; pdb.set_trace()
 def dijkstra(graph, start, end):
-    """
-    Dijkstra's algorithm Python implementation.
-
-    Arguments:
-        graph: Dictionnary of dictionnary (keys are vertices).
-        start: Start vertex.
-        end: End vertex.
-
-    Output:
-        List of vertices from the beggining to the end.
-
-    Example:
-
-    graph = {
-    ...     'A': {'B': 10, 'D': 4, 'F': 10},
-    ...     'B': {'E': 5, 'J': 10, 'I': 17},
-    ...     'C': {'A': 4, 'D': 10, 'E': 16},
-    ...     'D': {'F': 12, 'G': 21},
-    ...     'E': {'G': 4},
-    ...     'F': {'H': 3},
-    ...     'G': {'J': 3},
-    ...     'H': {'G': 3, 'J': 5},
-    ...     'I': {},
-    ...     'J': {'I': 8},
-    ... }    
-    >>> dijkstra(graph, 'C', 'I')
-    ['C', 'A', 'B', 'I']"""
 
     # Final distances dict
     D = {}
@@ -42,13 +14,16 @@ def dijkstra(graph, start, end):
     # The start vertex does not need to move and is reachable
     D[start] = 0 
 
-    # All nodes are unseen
+    # All nodes are unseen initially
     unseen_nodes = graph.keys()
 
     while len(unseen_nodes) > 0:
-        # Select the node with the lowest value in D (final distance)
+        
+
         shortest = None
         cur_node = ''
+
+        # Find the node with the shortest distance
         for temp_node in unseen_nodes:
             if shortest == None:
                 shortest = D[temp_node]
@@ -60,10 +35,11 @@ def dijkstra(graph, start, end):
         # Remove the selected node from unseen_nodes
         unseen_nodes.remove(cur_node)
 
-        # For each child (ie: connected vertex) of the current node
+        # For each child of the current node calc the distance
         for child_node, child_value in graph[cur_node].items():
             if D[child_node] < D[node] + child_value:
                 D[child_node] = D[node] + child_value
+
                 # To go to child_node, you have to go through node
                 P[child_node] = cur_node
 
@@ -73,15 +49,19 @@ def dijkstra(graph, start, end):
     # Begin from the end
     node = end
 
-    # While the current node is not the start node
+    # Search for the start node
     while not (node == start):
         if path.count(node) == 0:
-            path.insert(0, node) # Insert the predecessor of the current node
-            node = P[node] # The current node becomes its predecessor
+            # Insert the predecessor of the current node
+            path.insert(0, node) 
+
+            # The current node becomes its predecessor
+            node = P[node]
         else:
             break
 
-    path.insert(0, start) # Finally, insert the start vertex
+    # Finally, insert the start vertex to complete the path
+    path.insert(0, start) 
     return path
 
 
